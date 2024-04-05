febd6b5eed050cb14718e034f13a639ad8cb61ab
Ranierij <95530644+Ranierij@users.noreply.github.com>
2024-04-04 21:13:06 -0300

    Update Emprestimo.java

--- Emprestimo.java
+++ Emprestimo.java
@@ -2,20 +2,20 @@ package sc.senac.banco;
 
 public class Emprestimo {
     public static void main(String[] args) {
-        // Valores fornecidos diretamente
-        double valorEmprestimo = 1000.0; // Exemplo: R$ 1000,00
+        // Valores fornecidos diretamente (em centavos)
+        int valorEmprestimo = 100000; // Exemplo: R$ 1000,00 em centavos
         int meses = 12; // Exemplo: 12 meses
         double taxaJurosMensal = 0.02; // 2% ao mês
 
         // Calcula o valor da parcela usando a fórmula do juros composto
-        double valorParcela = calcularParcela(valorEmprestimo, taxaJurosMensal, meses);
+        int valorParcela = calcularParcela(valorEmprestimo, taxaJurosMensal, meses);
 
-        System.out.println("O valor da parcela é: R$ " + valorParcela);
+        System.out.println("O valor da parcela é: R$ " + (valorParcela / 100) + "," + (valorParcela % 100));
     }
 
     // Método para calcular o valor da parcela
-    public static double calcularParcela(double ve, double tx, int t) {
+    public static int calcularParcela(int ve, double tx, int t) {
         double vf = ve * Math.pow((1 + tx), t);
-        return vf / t; // Retorna o valor da parcela
+        return (int)(vf / t); // Retorna o valor da parcela em centavos
     }
 }
